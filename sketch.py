b011b3a5fa402a24a6bb89672e3f028967493129
anders <anders@anders.(none)>
2009-11-28 21:04:34 -0500

    refactored

--- sketch.py
+++ sketch.py
@@ -7,77 +7,136 @@ import time
 import Image, ImageOps, ImageDraw
 
 from pygame.locals import *
-pygame.init()
-window = pygame.display.set_mode(pygame.display.list_modes()[0])
-pygame.display.toggle_fullscreen()
-screen = pygame.display.get_surface()
 
-seconds = int(sys.argv[1])
-images = sys.argv[2:]
+
+red    = (255,0,0)
+black  = (0,0,0)
+white  = (255,255,255)
+orange = (255,128,0)
+yellow = (255,255,0)
 
 SWITCH  = 10
 TICK    = 11
-counter = 0
 
-current_image = None
-posterized = False
-posterize_bits = 4
-grayscale = False
-
-gridlines = 0
-
-def display():
-    global counter
-    global current_image, posterized
-    im = Image.open(images[random.randint(0, len(images) - 1)])
-    mode = im.mode
-    im.thumbnail(screen.get_size(),Image.ANTIALIAS)
-    assert mode in "RGB", "RGBA"
-    current_image = im
-    render(im)
-    counter = 0
-
-def render(im):
-    if grayscale:
-        im = ImageOps.grayscale(im)        
-        im = ImageOps.colorize(im,"black","white")    
-    if posterized:
-        im = ImageOps.autocontrast(im)                
-        im = ImageOps.posterize(im,posterize_bits)
-        im = ImageOps.autocontrast(im)
-    if gridlines > 0:
-        im = draw_grid(im,gridlines)
-
-    data = im.tostring()
-    surface = pygame.image.fromstring(data, im.size, im.mode)
-    screen.blit(surface,(0,0))
+def clear():
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    color = black
+    background.fill(color)
+    screen.blit(background, (0,0))
     pygame.display.flip()
 
-def make_grayscale():
-    global grayscale
-    if not grayscale:
-        grayscale = True
-    else:
-        grayscale = False
-    render(current_image)
 
-def posterize():
-    global posterized, posterize_bits
-    if not posterized:
-        posterized = True
-        posterize_bits = 4
-    else:
-        posterize_bits -= 1
-        if posterize_bits < 1:
-            posterized = False
-    render(current_image)
+class App:
+    def __init__(self):
+        self.counter = 0
 
-def grid():
-    global gridlines
-    gridlines += 1
-    if gridlines > 8:
-        gridlines = 1
-    render(current_image)
+        self.current_image = None
+        self.posterized = False
+        self.posterize_bits = 4
+        self.grayscale = False
+
+        self.gridlines = 0
+
+    def display(self):
+        clear()
+        im = Image.open(images[random.randint(0, len(images) - 1)])
+        mode = im.mode
+        im.thumbnail(screen.get_size(),Image.ANTIALIAS)
+        assert mode in "RGB", "RGBA"
+        self.current_image = im
+        self.render(im)
+        self.counter = 0
+
+    def skip(self):
+        # clear the timer
+        pygame.time.set_timer(SWITCH,0)
+        # display a different image
+        self.display()
+        # start the timer over fresh
+        pygame.time.set_timer(SWITCH,1000 * seconds)
+
+    def input(self,events):
+        for event in events: 
+            if event.type == QUIT: 
+                sys.exit(0) 
+            else:
+                if event.type == KEYDOWN and event.key == ord('q'):
+                    sys.exit(0)
+                if event.type == KEYDOWN and event.key == ord('s'):
+                    self.skip()
+                if event.type == KEYDOWN and event.key == ord('p'):
+                    self.posterize()
+                if event.type == KEYDOWN and event.key == ord('g'):
+                    self.make_grayscale()
+                if event.type == KEYDOWN and event.key == ord('l'):
+                    self.grid()                                
+                if event.type == SWITCH:
+                    self.display()
+                if event.type == TICK:
+                    self.tick()
+
+    def tick(self):
+        background = pygame.Surface((100,50))
+        background = background.convert()
+        color = white
+
+        if (seconds - self.counter) < 10:
+            color = yellow
+        if (seconds - self.counter) < 7:
+            color = orange
+        if (seconds - self.counter) < 3:
+            color = red
+        background.fill(color)
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(seconds - self.counter), 1, (10, 10, 10))
+
+        (sw,sh) = screen.get_size()
+        screen.blit(background, (sw - 100,sh - 50))
+        screen.blit(text, (sw - 90,sh - 40))
+        pygame.display.flip()
+        self.counter += 1
+
+    def make_grayscale(self):
+        if not self.grayscale:
+            self.grayscale = True
+        else:
+            self.grayscale = False
+        self.render(self.current_image)
+
+    def posterize(self):
+        if not self.posterized:
+            self.posterized = True
+            self.posterize_bits = 4
+        else:
+            self.posterize_bits -= 1
+            if self.posterize_bits < 1:
+                self.posterized = False
+        self.render(self.current_image)
+
+    def grid(self):
+        self.gridlines += 1
+        if self.gridlines > 8:
+            self.gridlines = 1
+        self.render(self.current_image)
+
+    def render(self,im):
+        im2 = im.copy()
+        if self.grayscale:
+            im2 = ImageOps.grayscale(im2)        
+            im2 = ImageOps.colorize(im2,"black","white")    
+        if self.posterized:
+            im2 = ImageOps.autocontrast(im2)                
+            im2 = ImageOps.posterize(im2,self.posterize_bits)
+            im2 = ImageOps.autocontrast(im2)
+        if self.gridlines > 0:
+            im2 = draw_grid(im2,self.gridlines)
+
+        data = im2.tostring()
+        surface = pygame.image.fromstring(data, im2.size, im2.mode)
+        screen.blit(surface,(0,0))
+        pygame.display.flip()
+
 
 def draw_grid(image,lines):
     if lines == 1: return image
@@ -88,9 +147,6 @@ def draw_grid(image,lines):
             draw.line([(0,x),(width,x)],fill="red")
         for x in segment(width,lines):
             draw.line([(x,0),(x,height)],fill="red")
-#        for c in range(lines):
-#            draw.line([(0,int(height/2)),(width,int(height/2))],fill="red")
-#            draw.line([(int(width/2),0),(int(width/2),height)],fill="red")
         return image
     except Exception, e:
         print "exception: " + str(e)
@@ -100,67 +156,23 @@ def segment(t,n):
         yield int((t/n) * i)
 
 
-def skip():
-    # clear the timer
-    pygame.time.set_timer(SWITCH,0)
-    # display a different image
-    display()
-    # start the timer over fresh
+if __name__ ==  "__main__":
+    pygame.init()
+    window = pygame.display.set_mode(pygame.display.list_modes()[0])
+    pygame.display.toggle_fullscreen()
+    screen = pygame.display.get_surface()
+    pygame.mouse.set_visible(False)
+
+    seconds = int(sys.argv[1])
+    images = sys.argv[2:]
+    clock = pygame.time.Clock()
     pygame.time.set_timer(SWITCH,1000 * seconds)
+    pygame.time.set_timer(TICK,1000)
 
-red    = (255,0,0)
-black  = (0,0,0)
-white  = (255,255,255)
-orange = (255,128,0)
-yellow = (255,255,0)
+    app = App()
+    app.display()
 
-def tick():
-    global counter
-    background = pygame.Surface((100,50))
-    background = background.convert()
-    color = white
-
-    if (seconds - counter) < 10:
-        color = yellow
-    if (seconds - counter) < 7:
-        color = orange
-    if (seconds - counter) < 3:
-        color = red
-    background.fill(color)
-    font = pygame.font.Font(None, 36)
-    text = font.render(str(seconds - counter), 1, (10, 10, 10))
-
-    screen.blit(background, (1500,1150))
-    screen.blit(text, (1510,1160))
-    pygame.display.flip()
-    counter += 1
-
-def input(events):
-    for event in events: 
-        if event.type == QUIT: 
-            sys.exit(0) 
-        else:
-            if event.type == KEYDOWN and event.key == ord('q'):
-                sys.exit(0)
-            if event.type == KEYDOWN and event.key == ord('s'):
-                skip()
-            if event.type == KEYDOWN and event.key == ord('p'):
-                posterize()
-            if event.type == KEYDOWN and event.key == ord('g'):
-                make_grayscale()
-            if event.type == KEYDOWN and event.key == ord('l'):
-                grid()                                
-            if event.type == SWITCH:
-                display()
-            if event.type == TICK:
-                tick()
-
-display()
-clock = pygame.time.Clock()
-pygame.time.set_timer(SWITCH,1000 * seconds)
-pygame.time.set_timer(TICK,1000)
-
-while True:
-    clock.tick(60)
-    input(pygame.event.get())
-    pygame.time.wait(100)
+    while True:
+        clock.tick(60)
+        app.input(pygame.event.get())
+        pygame.time.wait(100)
